02ff5e4b6d2f09e71397478fb350953ebd25ee94
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-03-22 18:06:07 +0200

    update to angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { StoreModule } from '@ngrx/store';
 import { MostModule } from '@themost/angular';
 import { AuthModule } from '@themost/angular';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,11 +18,11 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
     InfiniteScrollModule,
     BsDropdownModule.forRoot(),
-    StoreModule.forRoot({}),
     MostModule.forRoot({
       // use runkit endpoint
       base: 'http://localhost:8080/api/',
